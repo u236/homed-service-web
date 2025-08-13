0a6872d6b0af3d24ba9e48ee7e0d05917939c2aa
u236 <mail@u236.org>
2025-08-13 22:15:38 +0300

    add wb-ups

--- deploy/data/usr/share/homed-web/js/modbus.js
+++ deploy/data/usr/share/homed-web/js/modbus.js
@@ -15,6 +15,7 @@ class Modbus extends DeviceService
         wbMap12e:              'Wiren Board WB-MAP12E Energy Meter',
         wbMap12h:              'Wiren Board WB-MAP12H Energy Meter',
         wbMr6:                 'Wiren Board WB-MR6 Relay Controller',
+        wbUps:                 'Wiren Board UPS v3 Backup Power Supply',
         neptunSmartPlus:       'Neptun Smart+ Controller',
         jth2d1:                'JTH-2D1 Temperature and Humidity Sensor',
         t13:                   'T13-750W-12-H Frequency Converter'
